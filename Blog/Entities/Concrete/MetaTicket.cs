201da7b925f53b61c14dec4ac8a12df45d111b22
Ömer Faruk Sandıkçı <sandikci_omer@hotmail.com>
2023-04-15 19:32:54 +0300

    SEO

---
+++ Blog/Entities/Concrete/MetaTicket.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class MetaTicket : BaseEntity<int>, IEntity
+    {
+        public string Title { get; set; }
+        public string KeyWords { get; set; }
+        public string Description { get; set; }
+
+        public Article Article { get; set; }
+    }
+}
